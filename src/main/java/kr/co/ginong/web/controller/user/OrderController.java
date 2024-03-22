c6cce2298274136a12cd55bece1ba3c3e57702a7
JiniBean_hi <JiniBean@DESKTOP-2BGN6PD>
2024-03-22 18:23:49 +0900

    info, pay.html 작업중

---
+++ src/main/java/kr/co/ginong/web/controller/user/OrderController.java
@@ -0,0 +1,16 @@
+package kr.co.ginong.web.controller.user;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("user/order")
+public class OrderController {
+
+    @GetMapping("info")
+    public String info(){
+        return "user/order/info";
+    }
+    
+}
