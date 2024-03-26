6c903729ca4da49e3347acf94bb2fa1ae26d92b7
hky0715 <hky0715@gmail.com>
2024-03-26 16:07:50 +0900

    product view 생성, product repository, mapper 수정

---
+++ src/main/java/kr/co/ginong/web/entity/ProductView.java
@@ -0,0 +1,29 @@
+package kr.co.ginong.web.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class ProductView {
+    private long    id;
+    private String  name;
+    private int     price;
+    private int     weight;
+    private String  thumbnailName;
+    private String  thumbnailPath;
+    private String  exp;
+    private String  desc;
+    private boolean state;
+    private int     quantity;
+    private int     likeCount;
+
+    private long    categoryId;
+    private long    unitId;
+    private long    storageTypeId;
+    private long    adminId;
+}
--- src/main/java/kr/co/ginong/web/repository/ProductRepository.java
+++ src/main/java/kr/co/ginong/web/repository/ProductRepository.java
@@ -5,21 +5,23 @@ import java.util.List;
 import org.apache.ibatis.annotations.Mapper;
 
 import kr.co.ginong.web.entity.Product;
+import kr.co.ginong.web.entity.ProductView;
 
 @Mapper
 public interface ProductRepository {
 
-    List<Product> findAll(Long categoryId
+    List<ProductView> findAll(Long categoryId
                         , String query
                         , int offset
                         , int size);
 
     Product findById(Long id);
-    
+    int count(Long categoryId, String query);
+
+    /*
     void save(Product product);
     void update(Product product);
     void delete(long id);
 
-    int count(Long categoryId, String query);
-    
+    */
 }
