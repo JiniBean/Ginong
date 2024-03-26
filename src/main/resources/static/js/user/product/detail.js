32d8b8c5ca5857356444d1e481c81e9b2aef5063
minkyung16 <puppe316@naver.com>
2024-03-26 17:55:23 +0900

    관리자 상품 detail 수정중

---
+++ src/main/resources/static/js/user/product/detail.js
@@ -0,0 +1,11 @@
+
+
+window.addEventListener("load", function(){
+    var payBox = this.document.querySelector(".pay-box");
+    var payPop = this.document.querySelector(".pay-pop");
+
+    payBox.onclick = function(e) {
+        e.preventDefault();
+    };
+
+});
