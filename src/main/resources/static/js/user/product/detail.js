6569e18ae3bc222beb4bbca294a34fb5044833c0
minkyung16 <puppe316@naver.com>
2024-03-26 20:39:02 +0900

    상품 detail -+ 스크립트 작성

--- src/main/resources/static/js/user/product/detail.js
+++ src/main/resources/static/js/user/product/detail.js
@@ -1,11 +1,31 @@
-
-
 window.addEventListener("load", function(){
-    var payBox = this.document.querySelector(".pay-box");
-    var payPop = this.document.querySelector(".pay-pop");
 
-    payBox.onclick = function(e) {
+    var numberBox = this.document.querySelector(".numberBox");
+    var numberInput = numberBox.getElementsByClassName("number-input")[0];
+
+    numberBox.onclick = function (e) {
+
         e.preventDefault();
-    };
+
+        if(e.target.tagName!='A')
+            return;
+
+        var currentValue = parseInt(numberInput.value);
+        var state = e.target.dataset.btn;
+
+        switch (state) {
+            case 'minus' :
+                if(currentValue==1)
+                    return;
+                numberInput.value = currentValue-1;
+                break;
+            case 'plus' :
+                if(currentValue==10) //최대 재고수량까지
+                    return;
+                numberInput.value = currentValue+1;
+                break;
+        }
+
+    }
 
 });
