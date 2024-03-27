a63e46c26bf2eb2c07d6b5143e61b00bc3e6d64a
ssuukko <tlstpstpdl11@gmail.com>
2024-03-27 17:05:55 +0900

    controller 수정중

---
+++ src/main/java/kr/co/ginong/web/service/admin/ProductService.java
@@ -0,0 +1,15 @@
+package kr.co.ginong.web.service.admin;
+
+import kr.co.ginong.web.entity.Product;
+import kr.co.ginong.web.entity.ProductView;
+
+import java.util.List;
+
+public interface ProductService {
+    List<ProductView> getList();
+
+    void save(Product product);
+
+    void update(Product product);
+
+}
