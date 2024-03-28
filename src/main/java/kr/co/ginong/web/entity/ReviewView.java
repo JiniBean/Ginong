7076ab371ff63fd2c888a3dfe19efa044e05a610
hky0715 <hky0715@gmail.com>
2024-03-28 17:22:09 +0900

    후기 목록 View  관련 entity, mapper 수정

---
+++ src/main/java/kr/co/ginong/web/entity/ReviewView.java
@@ -0,0 +1,26 @@
+package kr.co.ginong.web.entity;
+
+import java.util.Date;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class ReviewView {
+    private long	id;
+    private Date	date;
+    private boolean	tagBest;
+    private boolean	tagFresh;
+    private boolean	tagThumbs;
+    private String 	content;
+    private long	memberId;
+    private long	productId;
+
+    private String  userName;
+    private String  productName;
+}
