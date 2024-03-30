e58bdec1b0fed9efeb15681e762a95291566f2c0
minkyung16 <puppe316@naver.com>
2024-03-30 19:17:54 +0900

    post 요청관련 csrf 설정

--- src/main/java/kr/co/ginong/web/config/WebSecurityConfig.java
+++ src/main/java/kr/co/ginong/web/config/WebSecurityConfig.java
@@ -10,7 +10,9 @@ public class WebSecurityConfig {
     
     @Bean
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
 		http
+		.csrf((csrf) -> csrf.disable())
 		.authorizeHttpRequests((requests) -> requests
 		.anyRequest().permitAll()
 		);
