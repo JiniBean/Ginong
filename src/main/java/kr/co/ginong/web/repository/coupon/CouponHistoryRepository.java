2363c6d988f942e5f94c8a1e6cb6718d802d3c2b
JiniBean_hi <JiniBean@DESKTOP-2BGN6PD>
2024-04-01 19:36:21 +0900

    CouponHistoryRepository entity 경로수정

--- src/main/java/kr/co/ginong/web/repository/coupon/CouponHistoryRepository.java
+++ src/main/java/kr/co/ginong/web/repository/coupon/CouponHistoryRepository.java
@@ -3,7 +3,7 @@ package kr.co.ginong.web.repository.coupon;
 import java.util.List;
 
 import org.apache.ibatis.annotations.Mapper;
-import kr.co.ginong.web.entity.cupon.CouponHistory;
+import kr.co.ginong.web.entity.coupon.CouponHistory;
 import kr.co.ginong.web.entity.order.Location;
 
 @Mapper
