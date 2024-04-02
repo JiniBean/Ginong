3b9341f4f0382e5ffcf3ebd10548fbed84924d03
seungwb <seungwb@gmail.com>
2024-04-02 21:22:01 +0900

    user/order/info 작업중

--- src/main/resources/static/js/user/order/info.js
+++ src/main/resources/static/js/user/order/info.js
@@ -29,8 +29,8 @@ document.addEventListener("DOMContentLoaded", function() {
 
 const form = document.getElementById("form");
 
-form.addEventListener("submit", (e) => {
-    e.preventDefault();
-    const formData = new FormData(form);
-    console.log(Object.fromEntries(formData));
-});
+// form.addEventListener("submit", (e) => {
+//     e.preventDefault();
+//     const formData = new FormData(form);
+//     console.log(Object.fromEntries(formData));
+// });
