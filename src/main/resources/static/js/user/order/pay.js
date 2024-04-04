54f98729137721b7d57e613c0a8edb32aec2f081
JiniBean <eunjini6645@gmail.com>
2024-04-04 23:49:01 +0900

    feat : order/pay 적립금 적용 완료
    1.적립금 유효성 검사
    2.적립금 계산 및 결제 금액 뿌려주기

--- src/main/resources/static/js/user/order/pay.js
+++ src/main/resources/static/js/user/order/pay.js
@@ -2,27 +2,36 @@ function formatNumber(num) {
     return num.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 }
 
-
 window.addEventListener("load", function () {
-    var couponSection = document.querySelector(".n-dropdown");      //드롭다운 전체 영역
-    var couponBtn = couponSection.querySelector("#dropdown-btn");   //드롭다운 버튼
-    var couponList = couponSection.querySelector("#dropdown-list"); //드롭다운 리스트
-    var couponInput = couponSection.querySelector(".coupon-input"); //드롭다운 선택 값 input(hidden)
+    let couponSection = document.querySelector(".n-dropdown");              //드롭다운 전체 영역
+    let couponBtn = couponSection.querySelector("#dropdown-btn");           //드롭다운 버튼
+    let couponList = couponSection.querySelector("#dropdown-list");         //드롭다운 리스트
+    let couponInput = couponSection.querySelector(".coupon-input");         //드롭다운 선택 값 input(hidden)
 
-    var totalSpan = document.querySelector("#total-price");         //총 상품금액 dataSet 영역
-    var paySummary = document.querySelector("#payment-summary");    //결제정보 섹션
-    var couponAmt = paySummary.querySelector(".coupon");            //쿠폰 적용 금액 영역
-    var pointAmt = paySummary.querySelector(".point");              //포인트 적용 금액 영역
-    var dlvryAmt = paySummary.querySelector(".dlvry")               //배송비 영역
-    var totalAmt = paySummary.querySelector(".total");              //총 결제 금액 영역
+    const pointSection = document.querySelector("#point-section");          //적립금 영역
+    const pointInput = pointSection.querySelector(".point-input");          //적립금 입력 칸
+    const remainPoint = pointSection.querySelector(".remain-point");        //사용자 보유 적립금
+    const usePointBtn = pointSection.querySelector(".use-point-btn");       //전액 사용 버튼
+    const minCheck = pointSection.querySelector(".min-check");              //최소 금액 유효성 검사 문구
+    const maxCheck = pointSection.querySelector(".max-check");              //최대 금액 유효성 검사 문구
 
-    var couponDisc; //쿠폰 할인 금액
-    var pointDisc;  //포인트 할인 금액
+    let totalSpan = document.querySelector("#total-price");                 //총 상품금액 dataSet 영역
+    let paySummary = document.querySelector("#payment-summary");            //결제정보 섹션
+    let couponAmt = paySummary.querySelector(".coupon");                    //쿠폰 적용 금액 영역
+    let pointAmt = paySummary.querySelector(".point");                      //포인트 적용 금액 영역
+    let dlvryAmt = paySummary.querySelector(".dlvry")                       //배송비 영역
+    let totalAmt = paySummary.querySelector(".total");                      //총 결제 금액 영역
+
+
+
+    let couponDisc; //쿠폰 할인 금액
+    let pointDisc;  //포인트 할인 금액
+
 
     //드롭다운 버튼 선택 했을 때
     couponBtn.onclick = function (e) {
         //쿠폰 리스트 보였다 안보였다
-        var active = couponList.classList.contains("active");
+        let active = couponList.classList.contains("active");
         if (active)
             couponList.classList.remove("active");
         else
@@ -46,10 +55,69 @@ window.addEventListener("load", function () {
 
         //쿠폰 가격 뿌려주기
         couponAmt.textContent = formatNumber(couponDisc);
-        total(couponDisc, 0);
+        total(couponDisc, pointDisc);
 
     }
 
+    pointInput.oninput = function (e) {
+
+        // 사용자가 입력한 포인트
+        let inputValue = parseInt(e.target.value);
+        let point = parseInt(remainPoint.dataset.point);
+        let check = validate(inputValue, point);
+        if(check){
+            //인풋 영역에 금액 뿌려주기
+            let remain = point-inputValue;
+            remainPoint.textContent = formatNumber(remain)+' P';
+            pointInput.value = inputValue;
+
+            //결제정보 영역에 적립금 뿌려주기
+            pointDisc = inputValue;
+            pointAmt.textContent = formatNumber(pointDisc);
+            total(couponDisc,pointDisc);
+        }
+    }
+
+    usePointBtn.onclick = function (e) {
+
+        let point = remainPoint.dataset.point;
+        let inputValue = point;
+
+        let check = validate(inputValue, point);
+        if(check){
+            //인풋 영역에 금액 뿌려주기
+            remainPoint.textContent = "0 P";
+            pointInput.value = point;
+
+            //결제정보 영역에 적립금 뿌려주기
+            pointDisc = inputValue;
+            pointAmt.textContent = formatNumber(pointDisc);
+            total(couponDisc,pointDisc);
+        }
+    };
+
+    // 적립금 1000원 이상, 잔여 적립금 이상 못 쓰게 Validation Check
+    function validate(inputValue, point){
+
+
+        if (inputValue < 1000){
+            minCheck.classList.remove("d:none");
+            maxCheck.classList.add("d:none");
+            return false;
+        }
+
+        if (inputValue > point){
+            minCheck.classList.add("d:none");
+            maxCheck.classList.remove("d:none");
+            return false;
+        }
+
+        if(1000 <= inputValue && inputValue <= point){
+            minCheck.classList.add("d:none");
+            maxCheck.classList.add("d:none");
+            return true;
+        }
+    }
 
     //총 결제금액 꽂아주기
     function total(couponDisc, pointDisc) {
@@ -60,61 +128,5 @@ window.addEventListener("load", function () {
 
         totalAmt.textContent = formatNumber(cost);
     }
-
-});
-
-window.addEventListener("load", function () {
-    const pointSection = document.querySelector("#point-section");
-    const pointInput = pointSection.querySelector(".point-input");
-    const remainPoint = pointSection.querySelector(".remain-point");
-
-    const usePointBtn = pointSection.querySelector(".use-point-btn");
-
-    const minCheck = pointSection.querySelector(".min-check");
-    const maxCheck = pointSection.querySelector(".max-check");
-
-    let point = remainPoint.dataset.point;
-
-    pointInput.oninput = function (e) {
-
-        // 사용자 잔여 포인트
-
-        // 사용자가 입력한 포인트
-        var inputValue = parseInt(e.target.value);
-        console.log("point = ", point);
-        console.log("userInputVal = ", inputValue)
-
-
-        // 포인트 1000원 이상, 잔여 포인트 이상 못 쓰게 Validation Check
-        if (inputValue < 1000){
-            minCheck.classList.remove("d:none");
-            maxCheck.classList.add("d:none");
-        }
-
-        if (inputValue > point){
-            minCheck.classList.add("d:none");
-            maxCheck.classList.remove("d:none");
-        }
-
-        if(1000 < inputValue && inputValue <= point){
-            minCheck.classList.add("d:none");
-            maxCheck.classList.add("d:none");
-        }
-
-    }
-
-    usePointBtn.onclick = function (e) {
-        if(point < 1000){
-            minCheck.classList.remove("d:none");
-            maxCheck.classList.add("d:none");
-            return;
-        }
-
-        remainPoint.textContent = "0 P";
-        pointInput.value = point;
-
-
-    };
-
 
 });
