6ca06eb7ef5e296ada1ae07a1e2454345d790d1b
seungwb <seungwb@gmail.com>
2024-04-08 16:43:56 +0900

    feat: 회원가입 css 문서들 제작
    [다수의 css 문서 제작]

--- src/main/java/kr/co/ginong/web/controller/user/MemberController.java
+++ src/main/java/kr/co/ginong/web/controller/user/MemberController.java
@@ -3,13 +3,19 @@ package kr.co.ginong.web.controller.user;
 import kr.co.ginong.web.service.user.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@RequestMapping("user")
 @Controller
 public class MemberController {
 
     @Autowired
     MemberService service;
+
+    @GetMapping("signin")
+    public String signin() {return "user/signin";}
+
 
 
 }
