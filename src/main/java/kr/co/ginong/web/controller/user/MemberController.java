ff8d26dd2c39138d025dc34a843cf64076459753
ssuukko <tlstpstpdl11@gmail.com>
2024-04-09 18:01:27 +0900

    fix: user/signup.html수정중
    [user/product/detail에서 후기, Q&A method명 수정하였습니다.]

--- src/main/java/kr/co/ginong/web/controller/user/MemberController.java
+++ src/main/java/kr/co/ginong/web/controller/user/MemberController.java
@@ -22,5 +22,16 @@ public class MemberController {
     @GetMapping("signup/index")
     public String index() {return "user/signup/index";}
 
+//    @GetMapping("signup1")
+//    public String signup() {return "user/signup/find-id";}
+//
+//    @GetMapping("signup2")
+//    public String signup2() {return "user/signup/confirm-id";}
+//
+//    @GetMapping("signup3")
+//    public String signup3() {return "user/signup/find-pwd";}
+//
+//    @GetMapping("signup4")
+//    public String signup4() {return "user/signup/change-pwd";}
 
 }
