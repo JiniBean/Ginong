7446dbf5a4eb376d6daa7965c409789eabd82c12
seungwb <seungwb@gmail.com>
2024-04-22 18:16:07 +0900

    refactor: Identity-email 수정 작업중
    [이메일 인증 기능에 필요한 기능을 추가중에 있습니다.]

--- src/main/resources/static/js/Identity-email.js
+++ src/main/resources/static/js/Identity-email.js
@@ -1,10 +1,20 @@
 document.addEventListener('DOMContentLoaded', function() {
+    const emailConfirm = document.querySelector('#email-confirm');
+    const verifyBtn = emailConfirm.querySelector('.verify_btn');
+
+    const emailSend = document.querySelector('#email-send');
+    const sendMailBtn = emailSend.querySelector('.send-mail-btn');
+
+    let timer; // 타이머 변수
+
     // 인증 확인 버튼 클릭 이벤트 리스너 추가
-    document.getElementById('verify_btn').addEventListener('click', function(event) {
-        event.preventDefault(); // 기본 동작 중단
+    verifyBtn.addEventListener('click', function(e) {
+        e.preventDefault(); // 기본 동작 중단
+        // 이전 타이머 제거
+        clearTimeout(timer);
 
         // 입력된 인증번호 가져오기
-        let verificationCode = document.querySelector('.verification-code').value;
+        let verificationCode = emailConfirm.querySelector('.verification-code').value;
 
         // 서버로 인증번호 전송
         let xhr = new XMLHttpRequest();
@@ -15,13 +25,13 @@ document.addEventListener('DOMContentLoaded', function() {
                     let response = JSON.parse(xhr.responseText);
                     if (response.success) {
                         // 인증이 확인되었을 때
-                        let verificationResult = document.querySelector('.verification-result');
+                        let verificationResult = emailConfirm.querySelector('.verification-result');
                         verificationResult.textContent = '인증이 확인되었습니다.';
                         verificationResult.style.color = 'green';
                         sessionStorage.setItem("verification-email","Y");
                     } else {
                         // 인증번호가 일치하지 않을 때
-                        let verificationResult = document.querySelector('.verification-result');
+                        let verificationResult = emailConfirm.querySelector('.verification-result');
                         verificationResult.textContent = '인증번호를 다시 한번 확인해주세요.';
                         verificationResult.style.color = 'red';
                     }
@@ -34,12 +44,13 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 
     // 이메일 발송 버튼 클릭 이벤트 리스너 추가
-    document.getElementById('send-mail-btn').addEventListener('click', function(event) {
-        event.preventDefault(); // 기본 동작 중단
+    sendMailBtn.addEventListener('click', function(e) {
+        e.preventDefault(); // 기본 동작 중단
 
+        //메일 보내기 버튼 눌렀을 때 재발송으로 텍스트 바꿈
+        sendMailBtn.textContent = '재발송';
         // 이메일 주소 가져오기
-        let email = document.querySelector('input[name="mail"]').value;
-
+        let email = emailSend.querySelector('input[name="mail"]').value;
         // 서버로 이메일 주소 전송
         let xhr = new XMLHttpRequest();
         xhr.open('POST', '/mailSend', true);
@@ -49,7 +60,13 @@ document.addEventListener('DOMContentLoaded', function() {
                 if (xhr.status === 200) {
                     let response = JSON.parse(xhr.responseText);
                     if (response.success) {
+                        //버튼의 비활성 요소 제거
+                        verifyBtn.classList.remove("disabled");
                         alert('이메일을 성공적으로 보냈습니다.');
+
+                        timer = setTimeout(function() {
+                            verifyBtn.classList.add("disabled");
+                        }, 15000); // 3분 (180,000밀리초) 15초 테스트중
                     } else {
                         alert('이메일 전송에 실패했습니다.');
                     }
