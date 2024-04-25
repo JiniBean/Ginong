0889092ea8aa063cdd597d64f836bbdf28c8da2a
hky0715 <hky0715@gmail.com>
2024-04-25 18:03:39 +0900

    Init: 주문View 생성에 따른 Repository, Mapper 생성 OrderViewRepository, OrderViewRepositoryMapper 생성

---
+++ src/main/java/kr/co/ginong/web/repository/order/OrderViewRepository.java
@@ -0,0 +1,13 @@
+package kr.co.ginong.web.repository.order;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import kr.co.ginong.web.entity.order.OrderView;
+
+@Mapper
+public interface OrderViewRepository {
+    List<OrderView> findAll(Long memberId);
+
+}
