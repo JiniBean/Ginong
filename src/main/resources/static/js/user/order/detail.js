a8ece45c64edb3b043e7ef7c680b83ba96ccb9df
Hong Kyuyoung <hky0715@gmail.com>
2024-04-29 15:55:41 +0900

    Design: detail.js 화면 변경 결제 정보 section 일부 변경 및 vue 적용

--- src/main/resources/static/js/user/order/detail.js
+++ src/main/resources/static/js/user/order/detail.js
@@ -2,17 +2,17 @@ const { createApp } = Vue
 const dummyResponse = {
     orderId: 2024042490646637,
     orderDate: '2024.04.25',
-    orderState: 1,
+    orderStatus: 3,
     orderItems: [
         { id: 1, productName: '텃밭 흙 감자', productPrice: 4500, quantity: 1 },
         { id: 2, productName: '직접 구해서 만든 어리굴젓 무침', productPrice: 58200, quantity: 1 },
         { id: 3, productName: '진짜 맛있는 오이고추장무침', productPrice: 9000, quantity: 1 }
     ],
-    location: [
-        { id: 1, receiverName: '기농이', receiverPhone: '010-1234-1234', addr1: '서울시 마포구 백범로 3', addr2: '3층 하이미디어'}
-    ],
+    location: {
+        id: 1, receiverName: '기농이', receiverPhone: '010-1234-1234', addr1: '서울시 마포구 백범로 3', addr2: '3층 하이미디어'
+    },
     payment: {
-        // ...
+        id: 1, deliveryFee: 2500, totalAmt: 26900, point: 1000, coupon: 1000
     },
     orderState: [
         { id: 1, name: '주문완료'},
@@ -20,7 +20,7 @@ const dummyResponse = {
         { id: 3, name: '배송중'},
         { id: 4, name: '배송완료'},
     ],
-    
+
 }
 
 createApp({
@@ -31,7 +31,7 @@ createApp({
         }
     },
     methods:{
-        
+        // 전체선택
         checkAll(e) {
             let checked = e.target.checked
             this.order.orderItems.forEach(item => item.checked = checked)
@@ -42,17 +42,9 @@ createApp({
         // let response = await fetch("/user/api/order/detail");
         // let list = await response.json();
         // this.list = list;
-        this.order = dummyResponse
+        // this.order = dummyResponse
+        this.order = { ...dummyResponse }
     },
-    // beforeCreate(){},
-    // beforeMount(){},
-    // mounted(){},
-    // beforeUpdate(){},
-    // updated(){},
-    // beforeUnmount(){},
-    // unmounted(){},
-    // activated(){},
-    // deactivated(){},
 }).mount('main');
 
 
