493de3c30ac2e252ea67ecae089e60e45225ed0d
GIGIJU_Desktop <xenon0625@gmail.com>
2024-05-06 01:18:51 +0900

    Feat: index 페이지 제작중

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -3,24 +3,14 @@ const { createApp } = Vue
 createApp({
     data() {
         return {
-            best: false,
-            sale: false,
-            pick: false,
+            // 베스트, 특가, 추천 중 기본 값 설정
+            selectedProduct: 'best',
         }
     },
     methods: {
-        selectItem(item) {
-            // 해당 항목을 클릭하면 해당 항목의 상태를 true로 설정하고 다른 항목의 상태를 false로 설정.
-            this[item] = true;
-            for (let key in this.$data) {
-                if (key !== item) {
-                    this[key] = false;
-                }
-            }
+        // 클릭 이벤트 발생 시, [베스트, 특가, 추천] 변경
+        selectProduct(product) {
+            this.selectedProduct = product;
         },
-
     },
 }).mount('main')
-
-
-
