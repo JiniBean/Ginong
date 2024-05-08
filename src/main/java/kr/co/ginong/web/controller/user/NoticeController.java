530580324e6fe781b5f76d1f23373970cb552a27
seungwb <seungwb@gmail.com>
2024-05-08 18:56:09 +0900

    feat: user/notice 퍼블리싱
    [NoticeController 생성
    notice/list.html 생성
    notice/detail.html 생성
    notice/detail.css 생성]

---
+++ src/main/java/kr/co/ginong/web/controller/user/NoticeController.java
@@ -0,0 +1,23 @@
+package kr.co.ginong.web.controller.user;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("notice")
+public class NoticeController {
+
+
+    @GetMapping("list")
+    public String list(){
+
+        return "/user/notice/list";
+    }
+
+    @GetMapping("detail")
+    public String detail(){
+
+        return "/user/notice/detail";
+    }
+}
