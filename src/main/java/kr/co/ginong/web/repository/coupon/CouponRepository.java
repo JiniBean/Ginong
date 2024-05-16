4c50a1ce91aaeaf025adf8f1e9c9a1e1a7b8fe4d
seungwb <seungwb@gmail.com>
2024-05-16 17:08:33 +0900

    feat: 1:1문의, 쿠폰의 레파지토리, 매퍼 생성
    [InquiryRepository.java 생성
    InquiryRepositoryMapper.java 생성
    CouponRepository.java 생성
    CouponRepositoryMapper.java 생성]

---
+++ src/main/java/kr/co/ginong/web/repository/coupon/CouponRepository.java
@@ -0,0 +1,8 @@
+package kr.co.ginong.web.repository.coupon;
+
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface CouponRepository {
+//    Integer countByMemberId(Long memberId);
+}
---
+++ src/main/java/kr/co/ginong/web/repository/mypage/InquiryRepository.java
@@ -0,0 +1,9 @@
+package kr.co.ginong.web.repository.mypage;
+
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface InquiryRepository {
+
+//    Integer countByMemberId(Long memberId);
+}
