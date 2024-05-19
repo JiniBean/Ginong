262d980b377d7f8d5d1d47bf71889d1c4b3b3c7d
minkyung16 <puppe316@naver.com>
2024-05-19 17:05:30 +0900

    feat: user 회원정보 수정영역 퍼블리싱 및 데이터 바인딩
    [/mypage/infoUpt, 기본정보/배송지 구분하여 데이터 바인딩]

---
+++ src/main/resources/static/js/user/mypage/info-update.js
@@ -0,0 +1,53 @@
+const { createApp } = Vue;
+
+createApp({
+    data() {
+        return{
+            currentTab : 'location',
+            birthDate : '',
+            memberList : [],
+            locationList : [],
+            defaultIctList : []
+        }
+    }
+    ,methods : {
+        formatDate(dateString) {
+            // 날짜 문자열을 Date 객체로 변환
+            const date = new Date(dateString);
+            // 년, 월, 일 추출
+            const year = date.getFullYear();
+            const month = String(date.getMonth() + 1).padStart(2, '0');
+            const day = String(date.getDate()).padStart(2, '0');
+            // yyyy-MM-dd 형식으로 반환
+            return `${year}-${month}-${day}`;
+        }
+    }
+    , async created(){
+
+        try {
+            // 세 개의 API 호출을 병렬로 실행
+            const [infoResponse,defaultIctResponse , lctResponse] = await Promise.all([
+                fetch("/api/member/userInfo"),
+                fetch("/api/member/location/defaultList"),
+                fetch("/api/member/location/list")
+            ]);
+
+            // 두 개의 응답을 JSON으로 변환
+            const [infoList, defaultIctList, lctList] = await Promise.all([
+                infoResponse.json(),
+                defaultIctResponse.json(),
+                lctResponse.json()
+            ]);
+
+            // 데이터를 Vue 인스턴스의 데이터 속성에 할당
+            this.memberList = infoList;
+            this.locationList = lctList;
+            this.defaultIctList = defaultIctList;
+
+            console.log("================", defaultIctList);
+
+        } catch (error) {
+            console.error("Failed to fetch data:", error);
+        }
+    }
+}).mount('main');
