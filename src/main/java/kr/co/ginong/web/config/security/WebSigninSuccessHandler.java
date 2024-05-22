6dbbcb379643b13e9ce2dd69f0f6cd5b4c69b218
GIGIJU_Desktop <xenon0625@gmail.com>
2024-05-22 17:08:19 +0900

    Fix: WebSigninSuccessHandler 원복 - 장바구니 기능 제거

--- src/main/java/kr/co/ginong/web/config/security/WebSigninSuccessHandler.java
+++ src/main/java/kr/co/ginong/web/config/security/WebSigninSuccessHandler.java
@@ -1,14 +1,9 @@
 package kr.co.ginong.web.config.security;
 
-import ch.qos.logback.core.net.SyslogOutputStream;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import kr.co.ginong.web.entity.cart.Cart;
 import kr.co.ginong.web.service.cart.CartService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
@@ -16,15 +11,8 @@ import org.springframework.security.web.authentication.SavedRequestAwareAuthenti
 import org.springframework.security.web.savedrequest.HttpSessionRequestCache;
 import org.springframework.security.web.savedrequest.SavedRequest;
 import org.springframework.stereotype.Component;
-import org.springframework.web.util.WebUtils;
 
 import java.io.IOException;
-import java.io.UnsupportedEncodingException;
-import java.net.URLDecoder;
-import java.nio.charset.StandardCharsets;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
 
 @Component
 public class WebSigninSuccessHandler extends SavedRequestAwareAuthenticationSuccessHandler {
@@ -49,8 +37,6 @@ public class WebSigninSuccessHandler extends SavedRequestAwareAuthenticationSucc
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
                                         Authentication authentication) throws IOException, ServletException {
 
-
-
         String remember = request.getParameter(REQUEST_PARAM_NAME);                             //signin 페이지에 아이디 저장 체크 박스를 누르면 "on"이 들어옴
         if ("on".equals(remember)) {                                                            //체크박스를 체크 했으면
             String username = ((WebUserDetails) authentication.getPrincipal()).getUsername();   //로그인시 사용했던 username 을 꺼내온다.
@@ -67,48 +53,6 @@ public class WebSigninSuccessHandler extends SavedRequestAwareAuthenticationSucc
         String targetUrl = determineTargetUrl(request);                                         //기존 가려고 했던 페이지로 로그인 완료 후 가기 위한 메소드 호출
         getRedirectStrategy().sendRedirect(request, response, targetUrl);                       //determineTargetUrl 에서 얻어온 url 로 리다이렉트 해준다.
 
-        // ######### 로그인 시 쿠키에 장바구니 데이터가 있다면 DB에 업데이트 하는 기능 ##############
-//        Long userId = ((WebUserDetails) authentication.getPrincipal()).getId();
-//
-//        // 로그인한 회원의 DB 장바구니 정보 가져오기
-//        List<Cart> cartListDB = cartService.getList(userId);
-//        System.out.println();
-//        System.out.println("#############################################");
-//        System.out.println("cartListDB.toString() =>" + cartListDB.toString());
-//        System.out.println("#############################################");
-//        System.out.println();
-//
-//        // 쿠키에서 장바구니 데이터 가져오기
-//        List<Cart> cartListCookie = getCartCookie(request, userId);
-//
-//        // DB의 장바구니 목록과 쿠키의 장바구니 목록을 비교 ( 기존 장바구니에 담겨있는 상품 = UPDATE / 새로 담은 상품 = INSERT )
-//        List<Cart> cartUpdate = new ArrayList<>();
-//        List<Cart> cartInsert = new ArrayList<>();
-//
-//        for (Cart cartCookie : cartListCookie) {
-//            boolean found = false;
-//            for (Cart cart : cartListDB) {
-//                if (cart.getProductId().equals(cartCookie.getProductId())) {
-//                    // DB에 이미 존재하는 항목이면 업데이트 목록에 추가
-//                    // cartDB.setQuantity(cartDB.getQuantity() + cartCookie.getQuantity());   기존 상품 개수 + 새로 담은 상품 개수
-//                    cart.setQuantity(cartCookie.getQuantity());                          // 새로 담은 값으로 덮어쓰기
-//                    cartUpdate.add(cart);
-//                    found = true;
-//                    break;
-//                }
-//            }
-//            if (!found) {
-//                // DB에 존재하지 않는 항목이면 삽입 목록에 추가
-//                cartInsert.add(cartCookie);
-//            }
-//        }
-//
-//        for(Cart cart : cartInsert) System.out.println(cart.toString());
-//        for(Cart cart : cartUpdate) System.out.println(cart.toString());
-
-        // ##############################################################################
-
-
         super.onAuthenticationSuccess(request, response, authentication);                       //onAuthenticationSuccess 에 정의 된 나머지 동작들을 수행
     }
 
@@ -123,40 +67,6 @@ public class WebSigninSuccessHandler extends SavedRequestAwareAuthenticationSucc
                                                                                                 //없다면 기본 페이지로 이동하도록 설정
         return targetUrl;
     }
-
-//    protected List<Cart> getCartCookie(HttpServletRequest request, Long userId) throws UnsupportedEncodingException, JsonProcessingException {
-//        Cookie[] cookies = request.getCookies();
-//        if (cookies == null) {
-//            System.err.println("쿠키가 없습니다");
-//            return null;
-//        }
-//
-//        for (Cookie cookie : cookies) {
-//            if ("cartList".equals(cookie.getName())) {
-//                // 쿠키 값을 디코딩
-//                String decodedValue = URLDecoder.decode(cookie.getValue(), StandardCharsets.UTF_8.name());
-//
-//                // JSON 문자열을 Map 리스트로 변환
-//                ObjectMapper objectMapper = new ObjectMapper();
-//                List<Map<String, Object>> cartDataList = objectMapper.readValue(decodedValue, new TypeReference<List<Map<String, Object>>>() {});
-//
-//                // Map 리스트를 Cart 객체 리스트로 변환
-//                List<Cart> cartList = new ArrayList<>();
-//                for (Map<String, Object> cartData : cartDataList) {
-//                    Cart cart = Cart.builder()
-//                            .productId(((Number) cartData.get("prdId")).longValue())
-//                            .quantity((Integer) cartData.get("quantity"))
-//                            .memberId(userId)
-//                            .build();
-//                    cartList.add(cart);
-//                }
-//                return cartList;
-//            }
-//        }
-//
-//        System.err.println("cartList 쿠키를 찾을 수 없습니다");
-//        return null;
-//    }
 
 
 }
