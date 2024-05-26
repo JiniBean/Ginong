6547f6bb4deb9fd7a7205ea45e44d8702d5c432e
GIGIJU_Desktop <xenon0625@gmail.com>
2024-05-26 18:37:21 +0900

    Feat: 관리자 상품리스트 재고 부분 수정

--- src/main/resources/static/js/admin/product/list.js
+++ src/main/resources/static/js/admin/product/list.js
@@ -1,6 +1,7 @@
 window.addEventListener("load", function () {
 
-    let checkAll = document.querySelector(".all"); //전체 선택 체크 박스
+    const checkAll = document.querySelector(".all"); //전체 선택 체크 박스
+    const stockList = document.querySelectorAll(".stock");
 
     //전체 선택 눌렀을 때
     checkAll.onclick = function () {
@@ -9,5 +10,15 @@ window.addEventListener("load", function () {
       // 전체 선택 버튼의 상태 값에 따라 모든 체크 박스 체크 상태 바꾸기
       checkboxes.forEach(check => check.checked = this.checked)
 
+    }
+
+    stockList.forEach((s) => {
+        s.addEventListener("click", move);
+    })
+
+    function move(e) {
+        e.preventDefault();
+        let id = e.target.dataset.id;
+        location.href = `/admin/stock/detail?p=${id}`;
     }
 })
