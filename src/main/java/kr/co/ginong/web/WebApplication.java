897d009ac678d40c25c4ede84fab1284272eecf6
hky0715 <hky0715@gmail.com>
2024-05-26 18:09:06 +0900

    Tomcat 배포를 위한 SpringBootServletInitializer 상속

--- src/main/java/kr/co/ginong/web/WebApplication.java
+++ src/main/java/kr/co/ginong/web/WebApplication.java
@@ -2,10 +2,14 @@ package kr.co.ginong.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class
-WebApplication {
+public class WebApplication extends SpringBootServletInitializer {
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(WebApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(WebApplication.class, args);
